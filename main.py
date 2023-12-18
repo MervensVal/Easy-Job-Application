fc3488316faaf2e7744d3784a8863b859350fd36
Mervens Val <valmervens@gmail.com>
2023-12-18 01:19:18 -0500

    added code

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from datetime import date
 import os
+import random
 
 try:
-    print("Thank you for applying with BuzCorp using our Quick App.")
+    print("Thank you for applying with BizCorp using our Quick App!")
     print('Please enter the following information:')
 
     today = date.today()
@@ -10,20 +11,28 @@ try:
     lname = input('Last Name: ')
     dob = input('Date of Birth (Format: MM-DD-YYYY): ')
     email = input('Email: ')
+    phone = input('Phone number (###-###-####): ')
     currPosition = input('Current Position: ')
     salary= int(input('Salary Expectations (must be a number): '))
     yrsExperience = int(input('Years of experience (minimum of 2 is required): '))
-    degreeLevel = input('Do you have at least a bachelors degree? Yes or No: ')[0]
+    degreeLevel = input('Do you have at least a bachelors degree? Yes (Y) or No (N): ')[0]
     degree = input('Degree: ')
 
     degreeLevel = degreeLevel.upper()
     month,day,year = map(int,dob.split("-"))
     age = today.year - year - ((today.month,today.day) < (month,day))
 
-    #check age, experience, degree
+    #check user input
     eligible = True
-    if (age < 18) or (yrsExperience < 2) or (degreeLevel != 'Y'):
+    if (len(fname)<0 or len(lname)<0  or len(dob)<0  or len(email)<0 or len(phone)<0  ):
         eligible = False
+        print('Error: First Name, Last Name, DOB, Email, and Phone are all required')
+    elif (email.count('@')== 0) or (email.count('.')== 0):
+        eligible = False
+        print('Error: invalid email')
+    elif (age < 18) or (yrsExperience < 2) or (degreeLevel != 'Y'):
+        eligible = False
+        print('Error: Age, Experience, or Degree Level does not meet minnimum requirements')
 
     #Create folder if not already exists
     if eligible:
@@ -31,13 +40,20 @@ try:
         isExist = os.path.exists(directory)
         if not isExist:
             os.mkdir(directory)
-        filePath = './'+directory+'/'+fname+lname+email[0]+dob[0:1]
+        fileName = fname+'_'+lname+'_'+str(random.randint(1,1000000))
+        filePath = './'+directory+'/'+fileName
         f = open(filePath,'x')
         f = open(filePath,'a')
-        f.write(fname + '|' + lname + '|' + dob + '|' + email + '|' + currPosition + '|' + str(salary) + '|' + str(yrsExperience)+ '|' + degree+ '|' + today.strftime("%m/%d/%Y, %H:%M:%S"))
-        f.close        
+        f.write(fname + '|' + lname + '|' + dob + '|' + email + '|' + phone + '|' + currPosition + '|' + str(salary) + '|' + str(yrsExperience)+ '|' + degree+ '|' + today.strftime("%m/%d/%Y, %H:%M:%S"))
+        f.close
+
+        #Save results in DB 
+        #Create table if does not exists
+        #insert data into table including filename
+        print("Information has been saved")
+
 except Exception as e:
     print('Error Occured')
     print('-----------------------------------------------------')
-    print(e)
+    print(e,'-',type(e).__name__)
     print('-----------------------------------------------------')
